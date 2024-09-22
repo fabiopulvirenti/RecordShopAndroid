200f5a4bf71f05098874a6c834697e9f357c4c28
fabiopulvirenti <fpulvirenti002@gmail.com>
2024-09-22 09:03:10 +0100

    feat:add activity_update_album

--- app/src/main/java/com/northcoders/recordshopandroid/ui/mainactivity/updatealbum/UpdateAlbumActivity.java
+++ app/src/main/java/com/northcoders/recordshopandroid/ui/mainactivity/updatealbum/UpdateAlbumActivity.java
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.northcoders.recordshopandroid.R;
 
-public class UpdateBookActivity extends AppCompatActivity {
+public class UpdateAlbumActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_update_book);
+        setContentView(R.layout.activity_update_album);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
